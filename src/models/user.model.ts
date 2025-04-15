e6690cb82ac6dc7dcf292a7a95774fe3b5735e32
GustavoFantin <gustavofantinbarros@gmail.com>
2025-04-15 11:07:42 -0700

    prettier installed and set up

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,21 +1,21 @@
-import mongoose, { Schema, Document } from "mongoose"
+import mongoose, { Schema, Document } from 'mongoose';
 
 export interface IUser extends Document {
-    username: string,
-    password: string,
-    message: string,
-    win: number,
-    lose: number,
-    scores: number
+  username: string;
+  password: string;
+  message: string;
+  win: number;
+  lose: number;
+  scores: number;
 }
 
 const userSchema: Schema = new Schema({
-    username: { type: String, required: true },
-    password: { type: String, required: true },
-    message: { type: String, required: true },
-    win: { type: Number, required: false },
-    lose: { type: Number, required: false },
-    scores: { type: Number, required: false }
-})
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  message: { type: String, required: true },
+  win: { type: Number, required: false },
+  lose: { type: Number, required: false },
+  scores: { type: Number, required: false },
+});
 
-export const User = mongoose.model<IUser>('User', userSchema)
+export const User = mongoose.model<IUser>('User', userSchema);
