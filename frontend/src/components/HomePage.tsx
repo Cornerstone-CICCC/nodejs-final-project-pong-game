ac439e36734a7577eea4c76148a2307c4d7c061a
GustavoFantin <gustavofantinbarros@gmail.com>
2025-04-17 12:35:37 -0700

    fixed background bugs

--- frontend/src/components/HomePage.tsx
+++ frontend/src/components/HomePage.tsx
@@ -1,8 +1,12 @@
 import { Link } from 'react-router-dom';
+import indexBg from '/indexBg.png';
 
 function HomePgae() {
   return (
-    <>
+    <section
+      className="text-white  h-full flex justify-center items-center bg-cover "
+      style={{ backgroundImage: `url(${indexBg})` }}
+    >
       <div className="min-h-screen bg-cover bg-center bg-no-repeat">
         <div className="max-w-7xl mx-auto px-4 py-16 text-center text-white">
           <h1 className="text-5xl sm:text-7xl md:text-8xl lg:text-9xl font-extrabold mb-6 text-center bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text text-transparent">
@@ -11,12 +15,12 @@ function HomePgae() {
           <h2 className="text-base sm:text-lg md:text-xl font-extrabold uppercase mb-12 text-gray-100 drop-shadow">
             Ready to Rally? Let the Ping Pong Battles Begin!
           </h2>
-          <div className="w-full aspect-video mb-16 flex items-center justify-center">
-            <div className="w-full aspect-video mb-16 flex items-center justify-center">
-              <div className="w-full h-full rounded-2xl p-1 bg-gradient-to-r from-pink-500 via-purple-500 to-violet-500 shadow-[0_0_30px_rgba(255,0,255,0.6)]">
-                <div className="w-full h-full bg-black rounded-xl overflow-hidden">
+          <div className="w-[full] lg:w-[55vw] aspect-video mb-16 flex items-center justify-center">
+            <div className="w-full lg:w-[55vw]  aspect-video mb-16 flex items-center justify-center">
+              <div className="w-full lg:w-[57vw] h-full rounded-2xl p-1 bg-gradient-to-r from-pink-500 via-purple-500 to-violet-500 shadow-[0_0_30px_rgba(255,0,255,0.6)]">
+                <div className="w-full lg:w-[55vw] h-full bg-black rounded-xl overflow-hidden">
                   <video
-                    className="w-full h-full object-cover"
+                    className="w-full lg:w-[55vw] h-full object-cover"
                     autoPlay
                     muted
                     loop
@@ -38,14 +42,14 @@ function HomePgae() {
             </Link>
           </div>
           <Link
-            className="text-lg font-bold text-gray-100 text-center mt-4 drop-shadow"
+            className="text-lg font-bold text-[#CED0CE] text-center mt-4 drop-shadow"
             to="/register"
           >
             Don't Have An Account Yet?
           </Link>
         </div>
       </div>
-    </>
+    </section>
   );
 }
 export default HomePgae;
--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -2,6 +2,7 @@ import { AiOutlineUnlock } from 'react-icons/ai';
 import { BiUser } from 'react-icons/bi';
 import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
+import DestopImage from '/background.jpg';
 
 const Register = () => {
   const [username, setUsername] = useState('');
@@ -46,81 +47,87 @@ const Register = () => {
   };
 
   return (
-    <div className="min-h-screen w-full flex justify-center items-center px-4">
-      <div className="bg-slate-800/30 border border-slate-400 rounded-md p-6 sm:p-8 shadow-lg backdrop-filter backdrop-blur-sm relative w-full max-w-md">
-        <h1 className="text-4xl text-white font-bold text-center mb-6">
-          Register
-        </h1>
-        <form onSubmit={handleRegister}>
-          <div className="relative my-10">
-            <input
-              type="text"
-              value={username}
-              onChange={e => setUsername(e.target.value)}
-              className="peer block w-full appearance-none border-0 border-b-2 border-gray-100 bg-transparent py-2.5 px-0 text-sm text-white focus:border-blue-600 focus:outline-none focus:ring-0"
-              placeholder=""
-              required
-            />
-            <label className="absolute text-sm text-white duration-200 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] left-0 peer-placeholder-shown:translate-y-2.5 peer-placeholder-shown:scale-100 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:text-blue-500">
-              Your Username
-            </label>
-            <BiUser className="absolute top-4 right-4" />
-          </div>
-
-          <div className="relative my-10">
-            <input
-              type="password"
-              value={password}
-              onChange={e => setPassword(e.target.value)}
-              className="peer block w-full appearance-none border-0 border-b-2 border-gray-100 bg-transparent py-2.5 px-0 text-sm text-white focus:border-blue-600 focus:outline-none focus:ring-0"
-              placeholder=""
-              required
-            />
-            <label className="absolute text-sm text-white duration-200 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] left-0 peer-placeholder-shown:translate-y-2.5 peer-placeholder-shown:scale-100 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:text-blue-500">
-              Your Password
-            </label>
-            <AiOutlineUnlock className="absolute top-4 right-4" />
-          </div>
-
-          <div className="relative my-10">
-            <input
-              type="password"
-              value={confirmPassword}
-              onChange={e => setConfirmPassword(e.target.value)}
-              className="peer block w-full appearance-none border-0 border-b-2 border-gray-100 bg-transparent py-2.5 px-0 text-sm text-white focus:border-blue-600 focus:outline-none focus:ring-0"
-              placeholder=""
-              required
-            />
-            <label className="absolute text-sm text-white duration-200 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] left-0 peer-placeholder-shown:translate-y-2.5 peer-placeholder-shown:scale-100 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:text-blue-500">
-              Confirm Password
-            </label>
-            <AiOutlineUnlock className="absolute top-4 right-4" />
-          </div>
-
-          {error && (
-            <div className="text-red-500 text-sm mb-4 text-center">{error}</div>
-          )}
-
-          <button
-            type="submit"
-            className="w-full mb-4 text-[18px] mt-6 rounded-full bg-white text-emerald-800 hover:bg-purple-700 hover:text-white py-2 px-6 font-semibold shadow-md transform transition-all duration-300 hover:scale-105 hover:shadow-[0_0_15px_4px_rgba(147,51,234,0.4)] active:scale-95 focus:outline-none focus:ring-2 focus:ring-purple-400 relative overflow-hidden"
-          >
-            <span className="inline-flex items-center gap-2 relative z-10">
-              <span>Register</span>
-              <span className="animate-pulse">🌟</span>
-            </span>
-            <span className="absolute inset-0 rounded-full bg-purple-500 opacity-20 animate-ping z-0" />
-          </button>
-
-          <div className="mt-4 text-sm text-center">
-            <span className="text-white mr-1">Already Created An Account?</span>
-            <Link className="text-blue-500 font-medium" to="/login">
-              Login
-            </Link>
-          </div>
-        </form>
+    <section
+      className="text-white  h-full flex justify-center items-center bg-cover "
+      style={{ backgroundImage: `url(${DestopImage})` }}
+    >
+      <div className="min-h-screen w-full flex justify-center items-center px-4">
+        <div className="bg-slate-800/30 border border-slate-400 rounded-md p-6 sm:p-8 shadow-lg backdrop-filter backdrop-blur-sm relative w-full max-w-md">
+          <h1 className="text-4xl text-white font-bold text-center mb-6">
+            Register
+          </h1>
+          <form onSubmit={handleRegister}>
+            <div className="relative my-10">
+              <input
+                type="text"
+                value={username}
+                onChange={e => setUsername(e.target.value)}
+                className="peer block w-full appearance-none border-0 border-b-2 border-gray-100 bg-transparent py-2.5 px-0 text-sm text-white focus:border-blue-600 focus:outline-none focus:ring-0"
+                placeholder=""
+                required
+              />
+              <label className="absolute text-sm text-white duration-200 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] left-0 peer-placeholder-shown:translate-y-2.5 peer-placeholder-shown:scale-100 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:text-blue-500">
+                Your Username
+              </label>
+              <BiUser className="absolute top-4 right-4" />
+            </div>
+            <div className="relative my-10">
+              <input
+                type="password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                className="peer block w-full appearance-none border-0 border-b-2 border-gray-100 bg-transparent py-2.5 px-0 text-sm text-white focus:border-blue-600 focus:outline-none focus:ring-0"
+                placeholder=""
+                required
+                minLength={8}
+              />
+              <label className="absolute text-sm text-white duration-200 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] left-0 peer-placeholder-shown:translate-y-2.5 peer-placeholder-shown:scale-100 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:text-blue-500">
+                Your Password
+              </label>
+              <AiOutlineUnlock className="absolute top-4 right-4" />
+            </div>
+            <div className="relative my-10">
+              <input
+                type="password"
+                value={confirmPassword}
+                onChange={e => setConfirmPassword(e.target.value)}
+                className="peer block w-full appearance-none border-0 border-b-2 border-gray-100 bg-transparent py-2.5 px-0 text-sm text-white focus:border-blue-600 focus:outline-none focus:ring-0"
+                placeholder=""
+                required
+                minLength={8}
+              />
+              <label className="absolute text-sm text-white duration-200 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] left-0 peer-placeholder-shown:translate-y-2.5 peer-placeholder-shown:scale-100 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:text-blue-500">
+                Confirm Password
+              </label>
+              <AiOutlineUnlock className="absolute top-4 right-4" />
+            </div>
+            {error && (
+              <div className="text-red-500 text-sm mb-4 text-center">
+                {error}
+              </div>
+            )}
+            <button
+              type="submit"
+              className="w-full mb-4 text-[18px] mt-6 rounded-full bg-white text-emerald-800 hover:bg-purple-700 hover:text-white py-2 px-6 font-semibold shadow-md transform transition-all duration-300 hover:scale-105 hover:shadow-[0_0_15px_4px_rgba(147,51,234,0.4)] active:scale-95 focus:outline-none focus:ring-2 focus:ring-purple-400 relative overflow-hidden"
+            >
+              <span className="inline-flex items-center gap-2 relative z-10">
+                <span>Register</span>
+                <span className="animate-pulse">🌟</span>
+              </span>
+              <span className="absolute inset-0 rounded-full bg-purple-500 opacity-20 animate-ping z-0" />
+            </button>
+            <div className="mt-4 text-sm text-center">
+              <span className="text-white mr-1">
+                Already Created An Account?
+              </span>
+              <Link className="text-blue-500 font-medium" to="/login">
+                Login
+              </Link>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
